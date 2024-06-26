5a6a7798f1324c7f894bd3bbaa056f68e8a05626
Lars Obist <lars@obist.de>
2024-06-26 20:51:29 +0200

    secrets improvements

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const { OpenAI } = require('openai');
 const { GoogleAuth } = require('google-auth-library');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 // MongoDB setup
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nbzpr4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nbzpr4r.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGO_APP_NAME}`;
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -24,12 +24,12 @@ const client = new MongoClient(uri, {
 const connectClient = async () => {
     if (!client.topology || !client.topology.isConnected()) {
         await client.connect();
-        console.log("Connected to MongoDB");
+        console.log("Connected to DB");
     }
 };
 
 const getCollection = (collectionName) => {
-    return client.db('chattaDatabase').collection(collectionName);
+    return client.db(process.env.MONGO_DB_NAME).collection(collectionName);
 };
 
 // User selection in-memory store

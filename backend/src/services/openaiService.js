cac52ae5fbc81919f6f501b1d69d5dd30b9e4d05
Lars Obist <lars@obist.de>
2024-09-19 21:51:17 +0200

    prompt verbesserungen

--- backend/src/services/openaiService.js
+++ backend/src/services/openaiService.js
@@ -17,17 +17,17 @@ const initializeMessageHistory = (language) => {
         You are a helpful room booking assistant for a company called chatta. Today's date is ${currentDate} and the 
         language you communicate is ${language}. Your main responsibility is to assist users with all aspects of room 
         reservations and call functions. This includes checking the availability of rooms, providing details on equipment 
-        options, and fulfilling requests. You can help with any related queries. The functions you can perform include:
+        options, and fulfilling requests. You can help with any related queries. The functions you can and must perform include:
         1. find_booking: Find a reservation with the given parameters or display all bookings if no parameters are provided.
         2. create_booking: Create a new reservation for a specified date and time. Bookings can only be made at the top of the hour 
-        (e.g., 10:00). If a user provides a time like "8" or "8:00," automatically transform it to "08:00" and validate it.
+        (e.g., 10:00). If a user provides a time like "8" or "8:00," automatically transform it to "08:00".
         3. delete_booking: Delete an existing reservation with the specified parameters.
         4. update_booking: Update an existing reservation with the given parameters.
-        5. get_available_rooms: List all rooms available to the user based on the specified parameters. Sometimes the user asks to create 
-        a booking with that data.All outputs must be in plain text only. Do not use any markdown formatting, such as **, bullet points (-) 
-        or numbered lists.If a user attempts to book a time slot that is not on the hour (e.g., 09:30), respond with an error message asking 
-        them to provide a valid time slot. When a request like creating a booking, updating, or deleting, send a validation message and call 
-        the function once the information is confirmed. If a user asks about anything outside the scope of room-related information, gently 
+        5. get_available_rooms: List all rooms available to the user. Ask if he wants to filter if no filter is given like equipment (Monitor)
+        All outputs must be in plain text only. Do not use any markdown formatting, such as **, bullet points (-) 
+        or numbered lists. If a user attempts to book a time slot that is not on the hour (e.g., 09:30), respond with an error message asking 
+        them to provide a valid time slot. When a request like creating a booking, updating, or deleting, send a validation message. 
+        Once the information is confirmed, call the function. If a user asks about anything outside the scope of room-related information, gently 
         remind them that your assistance is focused on room reservations and related services. Keep your answers very compact.
         `
     });
@@ -73,12 +73,12 @@ const handleOpenAIRequest = async (textInput, language) => {
             type: "function",
             function: {
                 name: "find_booking",
-                description: "Find a reservation with the given params or show all when no params are given",
+                description: "Find a reservation with the given params or show all when no params are given. Ask first if he wants to look for all or wants to filter.",
                 parameters: {
                     type: "object",
                     properties: {
                         date: { type: "string", description: "The date of the booking, e.g., 2024-06-26" },
-                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00, always transform to HH:00 format" },
+                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00 or 11, always transform to HH:00 format" },
                         roomNumber: { type: "string", description: "The room number for the booking" },
                         equipment: { type: "array", items: { type: "string" }, description: "List of equipment needed in the room" }
                     },
@@ -90,13 +90,13 @@ const handleOpenAIRequest = async (textInput, language) => {
             type: "function",
             function: {
                 name: "create_booking",
-                description: "Create a reservation with the params date and timeslot, after receiving that input, ask if they want a specific room based on the roomnumber or equipment. Only allow bookings at full hours (e.g., 09:00, 10:00). And return an answer with all booking values (Date, Time, Roomnumber, Equipment)",
+                description: "Create a reservation with the params date and timeslot, after receiving that input, ask if they want a specific room based on the roomnumber or equipment. Only allow bookings at full hours and parse them to the right format (9 and 9:00 should be converted to 09:00). And return an answer with all booking values (Date, Time, Roomnumber, Equipment)",
                 parameters: {
                     type: "object",
                     properties: {
                         roomNumber: { type: "string", description: "The room number for the booking" },
                         date: { type: "string", description: "The date of the booking, e.g., 2024-06-26" },
-                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00, always transform to HH:00 format" },
+                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00 or 11, always transform to HH:00 format" },
                         equipment: { type: "array", items: { type: "string" }, description: "List of equipment needed in the room" }
                     },
                     required: ["date", "timeSlot"]
@@ -107,13 +107,13 @@ const handleOpenAIRequest = async (textInput, language) => {
             type: "function",
             function: {
                 name: "delete_booking",
-                description: "Delete a reservation with the given params",
+                description: "Delete a reservation with the given params.",
                 parameters: {
                     type: "object",
                     properties: {
-                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 101" },
-                        date: { type: "string", description: "The date of the booking, e.g., 2024-06-26" },
-                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00, always in HH:00 format" }
+                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 100" },
+                        date: { type: "string", description: "The date of the booking, e.g., 2024-09-23" },
+                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00 or 11, always transform to HH:00 format" },
                     },
                     required: ["date", "roomNumber", "timeSlot"]
                 }
@@ -127,12 +127,12 @@ const handleOpenAIRequest = async (textInput, language) => {
                 parameters: {
                     type: "object",
                     properties: {
-                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 101" },
+                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 100" },
                         date: { type: "string", description: "The current date of the booking, e.g., 2024-06-26" },
-                        timeSlot: { type: "string", description: "The current time of the booking, e.g., 11:00, always transform to HH:00 format" },
-                        new_roomNumber: { type: "string", description: "The updated room number of the booking, e.g., 101" },
+                        timeSlot: { type: "string", description: "The time of the booking, e.g., 11:00 or 11, always transform to HH:00 format" },
+                        new_roomNumber: { type: "string", description: "The updated room number of the booking, e.g., 100" },
                         new_date: { type: "string", description: "The updated date of the booking, e.g., 2024-06-27" },
-                        new_timeSlot: { type: "string", description: "The updated time of the booking, e.g., 12:00, always transform to HH:00 format" }
+                        new_timeSlot: { type: "string", description: "The updated time of the booking, e.g., 12:00 or 12, always transform to HH:00 format" }
                     },
                     required: ["date", "timeSlot", "new_date", "new_timeSlot"]
                 }
@@ -142,13 +142,13 @@ const handleOpenAIRequest = async (textInput, language) => {
             type: "function",
             function: {
                 name: "get_available_rooms",
-                description: "List all rooms available to the user and proceed to create a reservation one after asking",
+                description: "List all rooms available to the user, before executing the function ask if he wants to filter if he hasnt specified a filter. Return all Room Data such as Roomnumber etc.",
                 parameters: {
                     type: "object",
                     properties: {
                         date: { type: "string", description: "The current date of the booking, e.g., 2024-06-26" },
-                        timeSlot: { type: "string", description: "The current time of the booking, e.g., 11:00, always transform to HH:00 formatf" },
-                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 101" },
+                        timeSlot: { type: "string", description: "The current time of the booking, e.g., 11:00, always transform to HH:00 format" },
+                        roomNumber: { type: "string", description: "The room number of the booking, e.g., 100" },
                         equipment: { type: "array", items: { type: "string" }, description: "List of equipment required, e.g., PC or Whiteboard" }
                     },
                     required: []
